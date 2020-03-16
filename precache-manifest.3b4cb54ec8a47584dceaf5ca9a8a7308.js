cdf6757908b6f593bd046510a1106d84240c292a
aynorica <amirdeilamizadeh1996@gmail.com>
2020-03-15 23:42:19 -0700

    Updates

---
+++ precache-manifest.3b4cb54ec8a47584dceaf5ca9a8a7308.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7531f96a8822188549a4fae716cafb",
+    "url": "/faceor/index.html"
+  },
+  {
+    "revision": "f807b1339a8a84ad6253",
+    "url": "/faceor/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "04a8af32535691bf3ed5",
+    "url": "/faceor/static/css/main.4fb0144b.chunk.css"
+  },
+  {
+    "revision": "f807b1339a8a84ad6253",
+    "url": "/faceor/static/js/2.5a5eecfd.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/faceor/static/js/2.5a5eecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a8af32535691bf3ed5",
+    "url": "/faceor/static/js/main.035bab7f.chunk.js"
+  },
+  {
+    "revision": "819ed4448c92e2b07295",
+    "url": "/faceor/static/js/runtime-main.f0e29c31.js"
+  },
+  {
+    "revision": "8923d8fb54ba0d96a8623be295a043f3",
+    "url": "/faceor/static/media/logo.8923d8fb.png"
+  }
+]);
